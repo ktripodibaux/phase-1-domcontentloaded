3a417a67c4f753fa63fb05c3c10ad562bb1c7645
Kurt Baux <ktripodibaux@gmail.com>
2022-06-08 17:01:29 -0400

    commit tree 270adf8e959d22b7bbb57dcdf2c2c59265a889b7

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const p = document.querySelector('p');
+// p.textContent = 'JavaScript is so cool. It lets me add text to my page programmatically.'
+
+document.addEventListener('DOMContentLoaded', () => {
+    p.textContent = 'This is really cool!'
+})
